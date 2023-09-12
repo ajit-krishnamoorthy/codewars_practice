d00698ae96184edb04426c74308bd535153b714a
Ajit Krishnamoorthy <ajit.krish18@gmail.com>
2023-09-11 20:30:20 -0500

    Daily kyu6 9/11

---
+++ 6kyu/whoLikesIt.py
@@ -0,0 +1,19 @@
+#Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+
+#[]                                -->  "no one likes this"
+#["Peter"]                         -->  "Peter likes this"
+#["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+#["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+#["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+
+def likes(names):
+    if len(names) == 0:
+        return "no one likes this"
+    elif len(names) == 1:
+        return names[0]+" likes this"
+    elif len(names) == 2:
+        return names[0]+" and "+names[1]+" like this"
+    elif len(names) == 3:
+        return names[0]+", "+names[1]+" and "+names[2]+" like this"
+    elif len(names) > 3:
+        return names[0]+", "+names[1]+" and "+str(len(names)-2)+" others like this"
